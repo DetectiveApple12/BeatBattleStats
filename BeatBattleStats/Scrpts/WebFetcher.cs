5c28d8f78d5cac218f4cbfff40623f0d6ccce9a3
Kos256 <kosainabro@gmail.com>
2026-05-21 18:40:28 +0500

    Create WebFetcher.cs

---
+++ BeatBattleStats/Scrpts/WebFetcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace BeatBattleStats.Scrpts
+{
+    public class WebFetcher
+    {
+        public HttpClient client;
+        public static string baseURL = "https://beat-battle.net/profile/{user}";
+
+        public WebFetcher()
+        {
+            client = new();
+            
+        }
+        
+        public async Task<string> FetchProfileHTML(string user)
+        {
+            var response = await client.GetAsync(baseURL.Replace("{user}", user));
+            var text = await response.Content.ReadAsStringAsync();
+            return text;
+        }
+    }
+}
